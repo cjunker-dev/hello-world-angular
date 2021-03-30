4ac0a12ae2fdfb47dbfe7266fa5d6ee783c1663a
Celina Junker <junkerc2@nku.edu>
2021-03-30 09:43:44 -0400

    learning angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'hello-world!!';
+  name: string = "Celina";
+  myText: string = "Default myText string";
+
+  changeName(): void {
+    this.name = "Celina J.";
+  }
 }
